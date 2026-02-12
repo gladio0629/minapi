5ea7f19facbe3538063e86b268c9a1c7ead1235a
sardor <sardorbekbekmuratovich@gmail.com>
2026-02-12 13:01:08 +0500

    created_at section succsessfully updated!

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, Field
 from uuid import uuid4
 from fastapi.middleware.cors import CORSMiddleware
-
+from datetime import datetime
 app = FastAPI(title="My FastAPI App", version="0.0.1")
 
 app.add_middleware(
@@ -16,16 +16,18 @@ app.add_middleware(
 DATABASE: dict[str,dict] = {}
 
 class PostCreate(BaseModel):
-    username: str = Field(title="Username", min_length=3, max_length=50)
-    password: str = Field(title="Password", min_length=3, max_length=50)
-    email: str = Field(title="Email", min_length=3, max_length=50)
+    username: str = Field(title="Username", min_length=3, max_length=100)
+    password: str = Field(title="Password", min_length=3, max_length=100)
+    email: str = Field(title="Email", min_length=3, max_length=100)
 
 class PostResponse(PostCreate):
     post_id:str
 
 @app.get("/")
 async def root():
-    return {"message": "Hello this is first Sardor's API! "}
+    return {"message": "Hello this is first Sardor's API! ",
+            'date': str(datetime.now()),
+            'author': 'mr_bekmuratov',}
 
 @app.get("/posts/{post_id}")
 async def get_post(post_id: str):
@@ -46,7 +48,10 @@ async def get_all_posts():
 async def post_create(post: PostCreate):
     post_id = str(uuid4())
     new_post = post.dict()
+    date = str(datetime.now())
+    date = date[:19].replace(' ', '-')
     new_post["post_id"] = post_id
+    new_post["created_at"] = date
     DATABASE[post_id] = new_post
     return new_post
 
